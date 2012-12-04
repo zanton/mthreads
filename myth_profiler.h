02be814ac89b1e1997176b787e25ae9e09c9b3c9
An Huynh <zanton.zzz@gmail.com>
2012-12-05 01:55:46 +0900

    deleted 2 lines

--- myth_profiler.h
+++ myth_profiler.h
@@ -49,8 +49,10 @@ void		profiler_init_thread(int rank);
 void		profiler_fini_thread(int rank);
 task_node_t profiler_create_new_node(task_node_t parent);
 void 		profiler_output_data();
+//void profiler_add_time_record(task_node_t node, char type, int worker);
 void 		profiler_add_time_start(task_node_t node, int worker, int start_code);
 void 		profiler_add_time_stop(task_node_t node, int worker, int stop_code);
+//void profiler_add_time_record_wthread(task_node_t node, char type, void * thread);
 task_node_t profiler_get_root_node();
 task_node_t profiler_get_sched_node(int i);
 
